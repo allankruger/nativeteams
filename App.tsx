664d0928fc3a4ad2b5fdeb29548abf7bd21d94ed
allankruger <allanfloreskruger@gmail.com>
2024-08-28 21:10:10 +0200

    feat: add theme file

--- App.tsx
+++ App.tsx
@@ -1,5 +1,12 @@
+import { ThemeProvider } from "styled-components";
 import Groups from "./src/screens/Groups";
 
+import theme from "./src/theme";
+
 export default function App() {
-  return <Groups />;
+  return (
+    <ThemeProvider theme={theme}>
+      <Groups />
+    </ThemeProvider>
+  );
 }
