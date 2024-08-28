20829c6a36fef1c486169ba7c85e54138ff51e07
allankruger <allanfloreskruger@gmail.com>
2024-08-28 20:34:07 +0200

    first commit

---
+++ src/screens/Groups/index.tsx
@@ -0,0 +1,13 @@
+import { StyleSheet, Text, View } from "react-native";
+
+const Groups = () => {
+  return (
+    <View>
+      <Text>Groups</Text>
+    </View>
+  );
+};
+
+export default Groups;
+
+const styles = StyleSheet.create({});
