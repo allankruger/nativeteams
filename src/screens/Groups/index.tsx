1e7d400b610784f33fdf735bcddf709ec74829fb
allankruger <allanfloreskruger@gmail.com>
2024-08-29 22:24:47 +0200

    feat: add TeamCard component - create component folder, index and its styles

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,12 +1,18 @@
 import { Container } from "./styles";
 import { Header } from "../../components/Header";
 import { ScreenTitle } from "../../components/ScreenTitle";
+import { TeamCard } from "../../components/TeamCard";
 
 export const Groups = () => {
   return (
     <Container>
       <Header />
       <ScreenTitle title="Teams" subtitle="play with your team" />
+      <TeamCard title="Team 1" />
+      <TeamCard title="Team 2" />
+      <TeamCard title="Team 3" />
+      <TeamCard title="Team 4" />
+      <TeamCard title="Team 5" />
     </Container>
   );
 };
